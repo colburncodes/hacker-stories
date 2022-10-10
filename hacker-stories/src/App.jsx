acadb63eed6f745c174a1499bee7da1d3924446f
colburncodes <colburnsanders@gmail.com>
2022-10-10 09:32:39 -0500

    arrow func expressions

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,15 +1,14 @@
 import List from "./components/List";
 import Search from "./components/Search";
 
-function App() {
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <Search />
-      <hr />
-      <List />
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List />
+  </div>
+);
+
 
 export default App;
